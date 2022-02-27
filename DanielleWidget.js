2b2f4ce8d08fba5ded6cbaa7b7cee7af4054417f
MyScriptableScripts <100395996+MyScriptableScripts@users.noreply.github.com>
2022-02-27 01:23:43 -0500

    Update DanielleWidget.js

--- DanielleWidget.js
+++ DanielleWidget.js
@@ -1,4 +1,26 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: magic;
-404: Not Found
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: magic;
+
+const url = "https://defiant-curse-mirror.glitch.me/Dani-lovins"
+
+
+const request = new Request(url);
+const response = await request.loadString()
+
+const widget = new ListWidget()
+widget.backgroundColor = Color.black()
+widget.textColor = Color.cyan()
+var text = widget.addText(response)
+text.centerAlignText()
+text.font = Font.ultraLightMonospacedSystemFont(20)
+
+if (!config.runsInWidget) {	    
+    await widget.presentMedium()	
+} else {    
+    Script.setWidget(widget)    
+    Script.complete()
+}
